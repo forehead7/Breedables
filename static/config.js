67780aeefc95cd6ef04213861186f70b12adfaa7
forehead7 <stuart.ross.7@gmail.com>
2017-01-03 21:16:20 +0000

    Update config.js

--- static/config.js
+++ static/config.js
@@ -10,8 +10,8 @@ var spreadsheetId = "1oDszV7lesCn0qTwGMEawsZIo9AhwQS7nsFXOreMWuZc";
 // ***********************************************************************************
 // If you don't have one worksheet, set the value to -1 to disable it
 var breedablesWorksheet = 1;
-var shiniesWorksheet = 2;
-var wantsWorksheet = 3;
+var shiniesWorksheet = -1;
+var wantsWorksheet = -1;
 
 // ***********************************************************************************
 // Your FC and IGN
